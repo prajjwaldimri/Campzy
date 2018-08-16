ac80121194b8d8cce4515a81b9b2100df5061484
Prajjwal <prajjwaldimri@hotmail.com>
2018-08-17 00:14:26 +0530

    Added postcss, clean-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: { main: './frontend/src/index.js' },
   output: {
     path: path.resolve(__dirname, './frontend/dist'),
-    filename: 'main.js',
+    filename: 'main.bundle.js',
   },
   module: {
     rules: [
@@ -15,6 +18,23 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.pug$/,
+        use: ['html-loader?attrs=false', 'pug-html-loader'],
+      },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin('frontend/dist', {}),
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'frontend/src/index.pug',
+      inject: false,
+    }),
+  ],
 };
