02a17612ab6d0e5f256eb84cfad9bfa6fa18ef9f
Prajjwal <prajjwaldimri@hotmail.com>
2018-08-16 23:29:55 +0530

    Added basic webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,18 @@ const path = require('path');
 module.exports = {
   entry: { main: './frontend/src/index.js' },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './frontend/dist'),
     filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
 };
