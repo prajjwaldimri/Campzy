b5907e4c807be9d043ee0f396ecd6e6ca15695cf
Prajjwal <prajjwaldimri@hotmail.com>
2018-08-17 23:26:31 +0530

    Updated user and camp schema

--- backend/models/camp.js
+++ backend/models/camp.js
@@ -4,6 +4,17 @@ const { Schema } = mongoose;
 
 const CampSchema = new Schema({
   name: String,
+  phone_number: String,
+  email: String,
+  location: String,
+  short_description: String,
+  long_description: String,
+  tags: [String],
+  amenities: [String],
+  services: [String],
+  places_of_interest: [String],
+  coordinates: [String],
+  packages: {},
   ownerId: String,
 });
 
--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,15 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   name: String,
+  email: String,
+  phone_number: String,
+  facebook: {
+    oauth_token: String,
+  },
+  google: {
+    oauth_token: String,
+  },
+  date_of_birth: Date,
 });
 
 module.exports = mongoose.model('User', UserSchema);
