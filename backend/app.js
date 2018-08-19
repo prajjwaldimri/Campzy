90e9eeb4282707e62acf04e586ea404ba3a8c088
Prajjwal <prajjwaldimri@hotmail.com>
2018-08-19 22:38:12 +0530

    Fixed Product Sans Fonts problem

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const { bodyParserGraphQL } = require('body-parser-graphql');
 const depthLimit = require('graphql-depth-limit');
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
+const path = require('path');
 const webpackConfig = require('../webpack.config.js');
 const schema = require('./schema/schema.js');
 require('./models/user');
@@ -31,9 +32,11 @@ if (process.env.ENVIRONMENT === 'development') {
   app.use(webpackMiddleware(webpack(webpackConfig)));
 } else {
   app.get('/', (req, res) => {
-    res.status(200).send('/frontend/dist/index.html');
+    res.status(200).sendFile(path.join(__dirname, '../frontend/dist/index.html'));
   });
 }
+
+app.use(express.static(path.join(__dirname, '../frontend/static')));
 
 app.use(
   '/graphql',
