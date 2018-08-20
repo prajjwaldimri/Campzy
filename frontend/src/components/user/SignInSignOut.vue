a88d8b6c07e3ffd57e461f614deb7b0f3b7977b2
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-08-20 14:30:04 +0530

    Demo Sign up card

--- frontend/src/components/user/SignInSignOut.vue
+++ frontend/src/components/user/SignInSignOut.vue
@@ -19,9 +19,27 @@
             a(href="#")
               v-avatar(size='32' style='margin:10px')
                 i.fab.fa-facebook.fa-2x
+      v-tab-item(id='signup')
+        v-card(flat)
+          form(v-model='valid' style='margin:10px')
+            v-text-field(v-model="phone" :rules='phoneRules' label='Phone Number' required)
+            v-text-field(v-model="email" :rules='emailRules' label='Email' required)
+            v-text-field(v-model="password" :rules='passwordRules' type='password' label='Password' required)
+          .text-xs-center(style='margin:10px;display:flex;flex-direction:column')
+            v-btn(round color='primary' @click.native="regUser()" dark) Sign Up
+            p Or Using
+          v-flex.text-xs-center
+            a(href='#')
+              v-avatar(size='32' style='margin:10px')
+                i.fab.fa-google.fa-2x
+            a(href="#")
+              v-avatar(size='32' style='margin:10px')
+                i.fab.fa-facebook.fa-2x
 
 </template>
 <script>
+import gql from 'vue-apollo';
+
 export default {
   data: () => ({
     valid: false,
@@ -34,8 +52,35 @@ export default {
     passwordRules: [
       v => !!v || 'Password is required',
     ],
+    phone: '',
+    phoneRules: [
+      v => !!v || 'Phone Number Required',
+    ],
 
   }),
+
+  methods: {
+    regUser() {
+      this.$apollo.mutate({
+        mutation: gql`mutation ($phone: String!, $email: String!, $password: String!){
+          register(phone: $phone, email: $email, password: $password){
+            phone
+            email
+            password
+          }
+        }`,
+        variables: {
+          phone: this.phone,
+          email: this.email,
+          password: this.password,
+        },
+      }).then((data) => {
+        console.log(data);
+      }).catch((error) => {
+        console.log(error);
+      });
+    },
+  },
 
 };
 </script>
