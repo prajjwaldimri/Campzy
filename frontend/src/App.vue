be1a1cd0ed2fa3ee866febe9f2d742f1b09e8019
Prajjwal <prajjwaldimri@hotmail.com>
2018-08-21 08:53:26 +0530

    Removed navbar from App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,11 @@
 <template lang="pug">
   v-app(dark)
-    navbar
     router-view
 </template>
 
 <script>
-import navbar from './components/Navbar.vue';
-
 export default {
   name: 'app',
-  components: {
-    navbar,
-  },
   data() {
     return {
       message: 'Hello, Vue!',
