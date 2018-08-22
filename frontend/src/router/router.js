2fb2df82a4acc6dbe148fd1c67c240700d82caed
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-08-22 15:19:07 +0530

    Removed unnecessary folders

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -2,10 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../components/Home.vue';
 import BookCampSite from '../components/user/BookCampSite.vue';
-import CampOwnerDashboard from '../components/camp_owner/CampOwnerDashboard.vue';
-import RegisterCamp from '../components/camp_owner/RegisterCamp.vue';
-import ManageCamps from '../components/admin/ManageCamps.vue';
-import AdminDashboard from '../components/admin/AdminDashboard.vue';
 import LoginRegister from '../components/user/LoginRegister.vue';
 import Settings from '../components/user/Settings.vue';
 import DashBoard from '../components/Dashboard.vue';
@@ -25,26 +21,6 @@ const router = new Router({
       path: '/bookCamp',
       name: 'book-camp',
       component: BookCampSite,
-    },
-    {
-      path: '/registerCamp',
-      name: 'register-camp',
-      component: RegisterCamp,
-    },
-    {
-      path: '/campownerDashboard',
-      name: 'camp-owner-dashboard',
-      component: CampOwnerDashboard,
-    },
-    {
-      path: '/manageCamps',
-      name: '/manage-camp',
-      component: ManageCamps,
-    },
-    {
-      path: '/adminDashboard',
-      name: '/admin-dashboard',
-      component: AdminDashboard,
     },
     {
       path: '/login',
