774d80da8963832bf4ac1793635404436ab2b14d
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-08-22 20:17:11 +0530

    Fixed css file problem in Dashboard

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,48 +1,55 @@
 <template lang="pug">
   div
-    v-navigation-drawer(:mini-variant.sync="mini" v-model="drawer" hide-overlay)
-      v-toolbar(flat)
-        v-list.pa-0
-          v-list-tile(avatar)
-            v-list-tile-avatar
-              img(src='https://randomuser.me/api/portraits/men/85.jpg')
-            v-list-tile-content
-              v-list-tile-title Ayush Bahuguna
+    navbar
+    div(style="margin-top:4.5rem")
+      v-navigation-drawer(:mini-variant.sync="mini" v-model="drawer" hide-overlay)
+        v-toolbar(flat)
+          v-list.pa-0
+            v-list-tile(avatar)
+              v-list-tile-avatar
+                img(src='https://randomuser.me/api/portraits/men/85.jpg')
+              v-list-tile-content
+                v-list-tile-title Ayush Bahuguna
+              v-list-tile-action
+                v-btn(icon @click.stop='mini=!mini')
+                  v-icon chevron_left
+        v-list.pt-0(dense)
+          v-divider
+          v-list-tile
             v-list-tile-action
-              v-btn(icon @click.stop='mini=!mini')
-                v-icon chevron_left
-      v-list.pt-0(dense)
-        v-divider
-        v-list-tile
-          v-list-tile-action
-            v-icon add
-          v-list-tile-content User Management
-        v-divider
-        v-list-tile
-          v-list-tile-action
-            v-icon add
-          v-list-tile-content Camp Management
-        v-divider
-        v-list-tile
-          v-list-tile-action
-            v-icon add
-          v-list-tile-content Camp Details
-        v-divider
-        v-list-tile
-          v-list-tile-action
-            v-icon add
-          v-list-tile-content Inventory
-        v-divider
-        v-list-tile
-          v-list-tile-action
-            v-icon add
-          v-list-tile-content Pricing
+              v-icon add
+            v-list-tile-content User Management
+          v-divider
+          v-list-tile
+            v-list-tile-action
+              v-icon add
+            v-list-tile-content Camp Management
+          v-divider
+          v-list-tile
+            v-list-tile-action
+              v-icon add
+            v-list-tile-content Camp Details
+          v-divider
+          v-list-tile
+            v-list-tile-action
+              v-icon add
+            v-list-tile-content Inventory
+          v-divider
+          v-list-tile
+            v-list-tile-action
+              v-icon add
+            v-list-tile-content Pricing
 
 
 </template>
 <script>
+import navbar from './Navbar.vue';
+
 export default {
   name: 'Dashboard',
+  components: {
+    navbar,
+  },
   data() {
     return {
       drawer: true,
