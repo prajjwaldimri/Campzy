a04d9b65fe91dd792208f1d0c860b2d1b60f0028
Prajjwal <prajjwaldimri@hotmail.com>
2018-08-25 22:11:52 +0530

    Added email verification dialog

--- frontend/src/components/user/EmailVerification.vue
+++ frontend/src/components/user/EmailVerification.vue
@@ -1,3 +1,42 @@
 <template lang="pug">
-  h1 Email Verification
+  v-dialog(v-model="verificationDialog" max-width="500px" v-if="verificationDialog")
+    v-card
+      v-card-title
+        span.headline Email Verification
+      v-card-text
+        v-text-field(v-model="token" label="Verification Token" prepend-icon="subject" clearable
+        hint="Enter the token you recieved in your email")
+      v-card-actions
+        v-spacer
+        v-btn(color="grey darken-1" flat @click.native="closeDialog")
+          | Cancel
+        v-btn(color="green" @click.native="saveCamp").white--text Save
+
 </template>
+
+<script>
+
+import { GraphQLClient } from 'graphql-request';
+import { EventBus } from '../../event-bus';
+
+export default {
+
+  data() {
+    return {
+      verificationDialog: true,
+      token: '',
+    };
+  },
+  mounted() {
+    EventBus.$on('open-email-verification-dialog', (id) => {
+      this.token = '';
+      this.verificationDialog = true;
+    });
+  },
+  methods: {
+    closeDialog() {
+      this.verificationDialog = false;
+    },
+  },
+};
+</script>
