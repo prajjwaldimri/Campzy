be0d8e9dce8ff856881237ed2215fa012824de95
Prajjwal <prajjwaldimri@hotmail.com>
2018-08-26 22:17:32 +0530

    Added eslint support to babel's dynamic imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   plugins: ['security'],
   extends: ['airbnb-base', 'plugin:vue/essential', 'plugin:security/recommended'],
+  parserOptions: {
+    parser: 'babel-eslint',
+    allowImportExportEverywhere: true,
+  },
 };
