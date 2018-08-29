b5dd5560474ae8fd9fc2a8b179e741b9dec01fb2
Prajjwal <prajjwaldimri@hotmail.com>
2018-08-29 11:03:52 +0530

    Improved 404 page

--- frontend/src/components/404.vue
+++ frontend/src/components/404.vue
@@ -2,7 +2,8 @@
   v-app(dark).notFound
     v-container.d-flex(align-center fluid)
       span.display-2.pl-1 Looks like you got lost
-      v-btn(large color="white" @click="$router.go(-1)").black--text.mt-3 Go Back
+      span.title.pl-2.pt-2.grey--text.text--lighten-1 We can't find what you are looking for
+      v-btn(large color="white" @click="$router.go(-1)").black--text.mt-4 Go Back
         v-icon.pl-2 history
 </template>
 
