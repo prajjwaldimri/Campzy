bf446ca83ec5b1e8a5b3702d627d1d688f47a784
Prajjwal <prajjwaldimri@hotmail.com>
2018-09-30 09:09:40 +0530

    Removed bookedBy, isBooked and added disabledDates to tent schema

--- backend/models/tent.js
+++ backend/models/tent.js
@@ -5,16 +5,12 @@ const { Schema } = mongoose;
 const TentSchema = new Schema({
   capacity: Number,
   type: String,
-  isBooked: { type: Boolean, default: false },
   bookingPrice: Number,
   preBookPeriod: Number,
   surgePrice: Number,
   camp: { type: Schema.Types.ObjectId, ref: 'Camp' },
-  bookedBy: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
   isAvailable: { type: Boolean, default: false },
+  disabledDates: { type: [Date] },
 });
 
 module.exports = mongoose.model('Tent', TentSchema);
