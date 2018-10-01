5bd966a66d2bbdad88a2d0dc6c67bcad4647b43a
Prajjwal <prajjwaldimri@hotmail.com>
2018-10-01 15:22:27 +0530

    Added array destructring

--- frontend/src/components/dashboard_components/blogger_dashboard/AddBlogs.vue
+++ frontend/src/components/dashboard_components/blogger_dashboard/AddBlogs.vue
@@ -93,8 +93,8 @@ export default {
             'Content-Type': 'multipart/form-data',
           },
         }).then((res) => {
-        console.log(res);
-        this.heroImage = res.data[0];
+        const [image] = res.data;
+        this.heroImage = image;
         this.saveBlog(this.heroImage);
       }).catch(() => {
         EventBus.$emit('show-error-notification-long', 'Failed to Upload');
