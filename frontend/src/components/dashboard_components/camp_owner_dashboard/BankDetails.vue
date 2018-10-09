53e7531c086348127dab1213005f0cd1bd735158
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-10-09 09:36:47 +0530

    Fixed padding in Statistics and bank details

--- frontend/src/components/dashboard_components/camp_owner_dashboard/BankDetails.vue
+++ frontend/src/components/dashboard_components/camp_owner_dashboard/BankDetails.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  v-container(fluid style='height:100%')
+  .bank-container
     h2.font-weight-bold.headline.text-uppercase Bank Details
     v-flex.mt-2(xs12 md6 style='max-width:100%')
       v-card.body-card(flat)
@@ -19,16 +19,22 @@
                 v-text-field(label='PAN Number')
         v-card-actions
           v-spacer
+          v-btn(dark) Clear
           v-btn(color='green' dark) Save
 </template>
 
 <style lang="scss" scoped>
+.bank-container {
+  padding: 2rem;
+  @media screen and (max-width: 960px) {
+    padding: 1rem;
+    padding-bottom: 10rem;
+  }
+  height: 100%;
+}
 .body-card {
   padding: 2rem;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
-  @media screen and (max-width: 960px) {
-    padding-bottom: 17rem;
-  }
 }
 </style>
