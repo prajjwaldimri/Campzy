88ea7ae4f317432540fd9be3b90cec2657a64850
Prajjwal <prajjwaldimri@hotmail.com>
2018-10-09 11:18:42 +0530

    Finished blog layout

--- frontend/src/components/blog/Blogs.vue
+++ frontend/src/components/blog/Blogs.vue
@@ -6,10 +6,19 @@
       h2.subheading.mt-2.grey--text.text--darken-2 Articles from your fellow Campzy members
 
       .blogs-grid.mt-4
-        v-card(v-for="(blog,index) in blogs" v-if="index % 5 === 0" dark).wide-card
-          v-card-title Wide
-        v-card(v-else).tall-card
-          v-card-title Tall
+        v-card(v-for="(blog,index) in blogs" v-if="index % 5 === 0" raised dark img="https://images.pexels.com/photos/1289363/pexels-photo-1289363.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260").wide-card
+          .card-container
+            v-card-title(primary-title)
+              h1 Title
+            v-card-actions
+              v-btn(light) Read the story
+
+        v-card(v-else raised dark img="https://images.pexels.com/photos/1464616/pexels-photo-1464616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260").tall-card
+          .card-container
+            v-card-title(primary-title)
+              h1 Title
+            v-card-actions
+              v-btn(light) Read the story
 
 </template>
 
@@ -42,11 +51,19 @@ export default {
 
 .wide-card {
   grid-column-end: span 4;
-  grid-row-end: span 8;
+  grid-row-end: span 10;
 }
 
 .tall-card {
   grid-column-end: span 2;
-  grid-row-end: span 10;
+  grid-row-end: span 12;
+}
+
+.card-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: 0 1rem 1rem 1rem;
+  justify-content: space-between;
 }
 </style>
