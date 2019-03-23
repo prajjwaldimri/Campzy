ba4ce724acfbd40f9b1e54b74c5a5b85c2752d61
Prajjwal <prajjwaldimri@hotmail.com>
2019-03-23 22:03:43 +0530

    Added / to sitemap

--- backend/sitemapGen.js
+++ backend/sitemapGen.js
@@ -4,6 +4,7 @@ const sitemap = sm.createSitemap({
   hostname: 'https://campzy.in',
   cacheTime: 600000,
   urls: [
+    { url: '/', changefreq: 'daily', priority: 0.3 },
     { url: '/search', changefreq: 'daily', priority: 0.3 },
     { url: '/blogs', changefreq: 'daily', priority: 0.3 },
     { url: '/termsAndConditions', changefreq: 'monthly', priority: 0.3 },
