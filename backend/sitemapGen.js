4997b7aa19fa2d8562531273062b9bab0c9ab108
Prajjwal <prajjwaldimri@hotmail.com>
2019-03-23 22:04:16 +0530

    Changed priority of /

--- backend/sitemapGen.js
+++ backend/sitemapGen.js
@@ -4,7 +4,7 @@ const sitemap = sm.createSitemap({
   hostname: 'https://campzy.in',
   cacheTime: 600000,
   urls: [
-    { url: '/', changefreq: 'daily', priority: 0.3 },
+    { url: '/', changefreq: 'daily', priority: 1.0 },
     { url: '/search', changefreq: 'daily', priority: 0.3 },
     { url: '/blogs', changefreq: 'daily', priority: 0.3 },
     { url: '/termsAndConditions', changefreq: 'monthly', priority: 0.3 },
