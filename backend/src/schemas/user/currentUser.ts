9da5e8c57ca4304ae5e4fd98e52e2c51374227c6
Prajjwal <prajjwaldimri@hotmail.com>
2019-05-18 20:20:34 +0530

    Adds currentUser

---
+++ backend/src/schemas/user/currentUser.ts
@@ -0,0 +1,24 @@
+import { Query, Resolver, Ctx } from "type-graphql";
+import { AuthenticationError } from "apollo-server-core";
+import { GraphContext } from "graphcontext";
+
+import { UserModel, User } from "../../models/user";
+import * as auth from "../../config/auth";
+
+@Resolver()
+export class CurrentUserResolver {
+  @Query(() => User)
+  private async getCurrentUser(@Ctx() ctx: GraphContext): Promise<any> {
+    try {
+      const loggedInUser = await auth.getAuthenticatedUser(ctx.req);
+
+      if (!loggedInUser) {
+        throw new AuthenticationError("You need to be logged in!");
+      }
+
+      return await UserModel.findById(loggedInUser.id);
+    } catch (err) {
+      return err;
+    }
+  }
+}
---
+++ backend/src/tests/user/currentUser.test.ts
@@ -0,0 +1,71 @@
+import { gCall } from "../test-utils/gCall";
+import faker from "faker";
+import { UserModel } from "../../models/user";
+
+describe("Current User Tests", (): void => {
+  jest.setTimeout(50000);
+
+  it("should return current user", async (): Promise<void> => {
+    const registerMutation = `
+    mutation CreateUserByEmail($data: CreateUserByEmailInput!) {
+      createUserByEmail(data: $data)
+    }
+    `;
+
+    const user = {
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: "validPassword"
+    };
+    const responseRegister = await gCall({
+      source: registerMutation,
+      variableValues: {
+        data: user
+      }
+    });
+
+    const jwtToken = responseRegister.data.createUserByEmail;
+
+    const currentUserQuery = `
+      query {
+        getCurrentUser {
+          name
+          email
+          type
+        }
+      }
+    `;
+
+    const response = await gCall({
+      source: currentUserQuery,
+      jwtToken
+    });
+
+    expect(response.data.getCurrentUser.name).toEqual(user.name);
+    expect(response.data.getCurrentUser.email).toEqual(
+      user.email.toLowerCase()
+    );
+    expect(response.data.getCurrentUser.type).toEqual("Camper");
+  });
+
+  it("should not return current user (Not logged in!)", async (): Promise<
+    void
+  > => {
+    const currentUserQuery = `
+      query {
+        getCurrentUser {
+          name
+          email
+          type
+        }
+      }
+    `;
+
+    const response = await gCall({
+      source: currentUserQuery,
+      jwtToken: faker.random.alphaNumeric(20).toString()
+    });
+
+    expect(response.errors).toBeDefined();
+  });
+});
