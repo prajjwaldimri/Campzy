4c12ee7a32b54866dd1845133a3998f7f285cb6e
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2019-05-20 23:58:01 +0530

    Added tests for addCamp query closed #17

--- backend/src/tests/camp/addCamp.test.ts
+++ backend/src/tests/camp/addCamp.test.ts
@@ -7,54 +7,46 @@ describe("Add Camp Tests", (): void => {
   jest.setTimeout(50000);
   const campPhoneNumber = `+919690046216`;
   const campTags = ["Mountain", "Leh", "Chopta"];
+
   it("should return camp", async (): Promise<void> => {
-    const LoginQuery = `
-    query login($data: LoginUserByEmailInput!) {
-    login(data: $data )
-  }
-  `;
     const passwordHash = await bcrypt.hash("validPassword", 12);
-    const user = await new UserModel({
+    const registerMutation = `
+    mutation CreateUserByEmail($data: CreateUserByEmailInput!) {
+      createUserByEmail(data: $data)
+    }
+    `;
+
+    const user = {
       name: faker.name.firstName(),
       email: faker.internet.email(),
       password: passwordHash
-    }).save();
+    };
+    const responseRegister = await gCall({
+      source: registerMutation,
+      variableValues: {
+        data: user
+      }
+    });
+    console.log(responseRegister);
 
     const dbUser = await UserModel.findOneAndUpdate(
       { email: user.email },
       { type: "Admin" }
     );
     expect(dbUser).toBeDefined();
-
-    const adminUser = await UserModel.findOne({ email: user.email });
-    console.log(adminUser);
-
-    const response = await gCall({
-      source: LoginQuery,
-      variableValues: {
-        data: {
-          email: user.email,
-          password: "validPassword"
-        }
-      }
-    });
-
-    console.log(response);
-    expect(response).toBeDefined();
-
-    const jwtToken = response.login;
-    console.log(jwtToken);
+    const jwtToken = responseRegister.data.createUserByEmail;
 
     const campOwner = await new UserModel({
       name: faker.name.firstName(),
       email: faker.internet.email(),
       password: passwordHash
     }).save();
+
     // Add Camp test
     const addCampMutation = `
     mutation addCamp($data: AddCampInput!) {
       addCamp(data: $data){
-        id
+        name
       }
     }
     `;
@@ -65,9 +57,8 @@ describe("Add Camp Tests", (): void => {
       url: faker.internet.url(),
       tags: campTags,
       location: "Chopta",
-      owernId: campOwner.id
+      ownerId: campOwner.id
     };
-    console.log(camp);
 
     const campResponse = await gCall({
       source: addCampMutation,
@@ -76,6 +67,43 @@ describe("Add Camp Tests", (): void => {
       },
       jwtToken
     });
-    console.log(campResponse);
+    expect(campResponse.data.addCamp.name).toEqual(campOwner.name);
+  });
+
+  it("should not return camp(Invlaid JWT)", async (): Promise<void> => {
+    const passwordHash = await bcrypt.hash("validPassword", 12);
+
+    const campOwner = await new UserModel({
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: passwordHash
+    }).save();
+
+    // Add Camp test
+    const addCampMutation = `
+    mutation addCamp($data: AddCampInput!) {
+      addCamp(data: $data){
+        name
+      }
+    }
+    `;
+    const camp = {
+      name: faker.name.findName(),
+      phoneNumber: campPhoneNumber,
+      email: faker.internet.email(),
+      url: faker.internet.url(),
+      tags: campTags,
+      location: "Chopta",
+      ownerId: campOwner.id
+    };
+
+    const campResponse = await gCall({
+      source: addCampMutation,
+      variableValues: {
+        data: camp
+      },
+      jwtToken: faker.random.alphaNumeric(20).toString()
+    });
+    expect(campResponse.errors).toBeDefined();
   });
 });
