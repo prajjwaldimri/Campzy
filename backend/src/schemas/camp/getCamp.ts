ed52b6ed9b2e008570554665a245d5587bf74f5e
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2019-05-26 11:02:59 +0530

    Converted getCamp query into typescript

---
+++ backend/src/schemas/camp/getCamp.ts
@@ -0,0 +1,35 @@
+import { Resolver, Ctx, Query, Arg } from "type-graphql";
+import { AuthenticationError } from "apollo-server-core";
+import { GraphContext } from "graphcontext";
+
+import { CampModel, Camp } from "../../models/camp";
+import { UserModel } from "../../models/user";
+import * as auth from "../../config/auth";
+
+@Resolver()
+export class GetCampResolver {
+  @Query(() => Camp)
+  private async getCamp(
+    @Ctx() ctx: GraphContext,
+    @Arg("id") id: string
+  ): Promise<any> {
+    try {
+      const user = await auth.getAuthenticatedUser(ctx.req);
+      const userData = await UserModel.findById(user.id).exec();
+      if (!userData) {
+        throw new AuthenticationError("You need to be logged in!");
+      }
+      const isUserAdmin = auth.isUserAdmin(userData);
+
+      if (!isUserAdmin) {
+        throw new AuthenticationError("You are not authorised for this!");
+      }
+
+      return await CampModel.findById(id)
+        .populate("ownerId", "name")
+        .exec();
+    } catch (err) {
+      return err;
+    }
+  }
+}
