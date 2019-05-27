c6d2d3e8d65e6e3ff9ae7a769dfb868b71545e74
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2019-05-27 21:01:34 +0530

    Converted countCamps query into Typescript

---
+++ backend/src/schemas/camp/countCamps.ts
@@ -0,0 +1,32 @@
+import { Resolver, Ctx, Mutation, InputType, Field, Arg } from "type-graphql";
+import { AuthenticationError } from "apollo-server-core";
+import { GraphContext } from "graphcontext";
+
+import { CampModel, Camp } from "../../models/camp";
+import { UserModel } from "../../models/user";
+import * as auth from "../../config/auth";
+
+@Resolver()
+export class AddCampResolver {
+  @Mutation(() => Camp)
+  private async countCamps(@Ctx() ctx: GraphContext): Promise<any> {
+    try {
+      const user = await auth.getAuthenticatedUser(ctx.req);
+      const userData = await UserModel.findById(user.id).exec();
+      if (!userData) {
+        throw new AuthenticationError("You need to be logged in!");
+      }
+      const isUserAdmin = auth.isUserAdmin(userData);
+
+      if (!isUserAdmin) {
+        throw new AuthenticationError("You are not authorised for this!");
+      }
+      const count = await CampModel.estimatedDocumentCount({});
+      return {
+        count
+      };
+    } catch (err) {
+      return err;
+    }
+  }
+}
