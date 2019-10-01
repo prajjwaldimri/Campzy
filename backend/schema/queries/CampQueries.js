ab317f121c42714490b9555959c49569b5a8e206
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2019-10-01 12:39:49 +0530

    minor changes

--- backend/schema/queries/CampQueries.js
+++ backend/schema/queries/CampQueries.js
@@ -467,7 +467,8 @@ const getCampsInPlace = {
           },
         });
       await forEach(results, async (result) => {
-        console.log(result.inventory[0])
+        console.log(result)
+        console.log("next Camp")
         if (result.inventory[0]) {
           if (result.inventory[0].bookingPrice > 40000) {
             console.log("luxury camp")
